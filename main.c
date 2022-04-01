f8b11db78a6a513bbe155944ce18b1fe7e09706c
edurbp <39468080+edurbp@users.noreply.github.com>
2022-04-01 20:38:47 +0000

    New upload V1

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+*  @file Maquinas.h
+ * @author Eduardo
+ * @email a18589@alunos.ipca.pt
+ * @date 2022
+*/
+
+#include "Maquinas.h"
+#include "job.h"
+
+int main() {
+    Maquina* m1 = CriaMaquina(1, "m1");
+    Maquina* m2 = CriaMaquina(2, "m2");
+    Maquina* m3 = CriaMaquina(3, "m3");
+    Maquina* m4 = CriaMaquina(4, "m4");
+    Maquina* m5 = CriaMaquina(5, "m5");
+    Maquina* m6 = CriaMaquina(6, "m6");
+    Maquina* m7 = CriaMaquina(7, "m7");
+    Maquina* m8 = CriaMaquina(8, "m8");
+    Maquina* m9 = CriaMaquina(9, "m9");
+
+    //Job* j1 = CriaJob(1, 3, operacoes);
+}
